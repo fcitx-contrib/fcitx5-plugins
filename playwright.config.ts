688537cde56c84f8186d3a19405f3c841fd33211
Qijia Liu <liumeo@pku.edu.cn>
2025-10-24 19:35:34 -0400

    fix download artifact

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: 'tests',
+  retries: 3,
+  expect: {
+    timeout: 10000,
+  },
   fullyParallel: true,
   webServer: {
     command: 'npx serve -l 9000 -S fcitx5-online',

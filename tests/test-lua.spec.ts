01004ccc26950ddf6d9c3954de08c2a8df01349e
Qijia Liu <liumeo@pku.edu.cn>
2026-01-31 14:24:45 -0500

    fix test

--- tests/test-lua.spec.ts
+++ tests/test-lua.spec.ts
@@ -4,6 +4,6 @@ import { expectText, init } from './util'
 test('Date', async ({ page }) => {
   await init(page, ['chinese-addons', 'lua'], '拼音')
 
-  await page.keyboard.type('riqi3')
+  await page.keyboard.type('riqi2')
   await expectText(page, /\d+-\d+-\d+/)
 })
